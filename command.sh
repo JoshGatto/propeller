409077b0b346a330ebd04f426871fd6ab7d195fa
Josh Gatto <joshgatto20@gmail.com>
2023-01-11 10:55:04 +1100

    add command.sh

---
+++ command.sh
@@ -0,0 +1,20 @@
+npx create-react-app propeller
+
+cd propeller
+
+npm start
+
+gh repo create 
+
+git switch -C update_logo
+
+git add .
+
+git commit -m "update logo & link"
+
+git push --set-upstream origin update_logo
+
+gh pr create
+
+gh pr merge
+
